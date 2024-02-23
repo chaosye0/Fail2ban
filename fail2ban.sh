fcbac455a75bdb665c70767bc2e1a5fcd1dc91c5
chaosye0 <30192801+chaosye0@users.noreply.github.com>
2024-02-23 21:39:12 +0800

    Update fail2ban.sh

--- fail2ban.sh
+++ fail2ban.sh
@@ -6,50 +6,13 @@ clear
 #ReadSSHPort
 [ -z "`grep ^Port /etc/ssh/sshd_config`" ] && ssh_port=22 || ssh_port=`grep ^Port /etc/ssh/sshd_config | awk '{print $2}'`
 
-#CheckOS
-if [ -n "$(grep 'Aliyun Linux release' /etc/issue)" -o -e /etc/redhat-release ]; then
-  OS=CentOS
-  [ -n "$(grep ' 7\.' /etc/redhat-release)" ] && CentOS_RHEL_version=7
-  [ -n "$(grep ' 6\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release6 15' /etc/issue)" ] && CentOS_RHEL_version=6
-  [ -n "$(grep ' 5\.' /etc/redhat-release)" -o -n "$(grep 'Aliyun Linux release5' /etc/issue)" ] && CentOS_RHEL_version=5
-elif [ -n "$(grep 'Amazon Linux AMI release' /etc/issue)" -o -e /etc/system-release ]; then
-  OS=CentOS
-  CentOS_RHEL_version=6
-elif [ -n "$(grep 'bian' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Debian" ]; then
-  OS=Debian
-  [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
-  Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
-elif [ -n "$(grep 'Deepin' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Deepin" ]; then
-  OS=Debian
-  [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
-  Debian_version=$(lsb_release -sr | awk -F. '{print $1}')
-# kali rolling
-elif [ -n "$(grep 'Kali GNU/Linux Rolling' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Kali" ]; then
-  OS=Debian
-  [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
-  if [ -n "$(grep 'VERSION="2016.*"' /etc/os-release)" ]; then
-    Debian_version=8
-  else
-    echo "${CFAILURE}Does not support this OS, Please contact the author! ${CEND}"
-    kill -9 $$
-  fi
-elif [ -n "$(grep 'Ubuntu' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == "Ubuntu" -o -n "$(grep 'Linux Mint' /etc/issue)" ]; then
-  OS=Ubuntu
-  [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
-  Ubuntu_version=$(lsb_release -sr | awk -F. '{print $1}')
-  [ -n "$(grep 'Linux Mint 18' /etc/issue)" ] && Ubuntu_version=16
-elif [ -n "$(grep 'elementary' /etc/issue)" -o "$(lsb_release -is 2>/dev/null)" == 'elementary' ]; then
-  OS=Ubuntu
-  [ ! -e "$(which lsb_release)" ] && { apt-get -y update; apt-get -y install lsb-release; clear; }
-  Ubuntu_version=16
-else
-  echo "${CFAILURE}Does not support this OS, Please contact the author! ${CEND}"
-  kill -9 $$
-fi
+apt-get -y update
+apt-get -y install lsb-release
+
 #Read Imformation From The User
 echo "Welcome to Fail2ban!"
 echo "--------------------"
-echo "This Shell Script can protect your server from SSH attacks with the help of Fail2ban and iptables"
+echo "This Shell Script only support ubuntu 20.04!!!"
 echo ""
 
 while :; do echo
@@ -91,16 +54,12 @@ if [ ${bantime} == '' ];then
 	bantime=24
 fi
 ((bantime=$bantime*60*60))
-#Install
-if [ ${OS} == CentOS ]; then
-  yum -y install epel-release
-  yum -y install fail2ban
-fi
 
-if [ ${OS} == Ubuntu ] || [ ${OS} == Debian ];then
+#Install
+
+
   apt-get -y update
   apt-get -y install fail2ban
-fi
 
 #Configure
 rm -rf /etc/fail2ban/jail.local
@@ -142,31 +101,15 @@ EOF
 fi
 
 #Start
-if [ ${OS} == CentOS ]; then
-  if [ ${CentOS_RHEL_version} == 7 ]; then
     systemctl restart fail2ban
     systemctl enable fail2ban
-  else
-    service fail2ban restart
-    chkconfig fail2ban on
-  fi
-fi
-
-if [[ ${OS} =~ ^Ubuntu$|^Debian$ ]]; then
-  service fail2ban restart
-fi
 
 #Finish
 echo "Finish Installing ! Reboot the sshd now !"
 
-if [[ ${OS} =~ ^Ubuntu$|^Debian$ ]]; then
     systemctl restart sshd
-  else
-    service ssh restart
-fi
 
 
 echo ""
-
 
 echo "Fail2ban is now runing on this server now!"
